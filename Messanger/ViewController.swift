dcaa652f20f409046e7a7629d960e26a6162d525
Aldrin <gamaaldrin@gmail.com>
2020-11-16 18:30:29 +0200

    Getting started

---
+++ Messanger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messanger
+//
+//  Created by Aldrin Bahle Gama on 2020/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
